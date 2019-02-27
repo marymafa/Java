3f43e32fae736dec35389fd3f2216c47930d9a13
TCGPC1 <you@example.com>
2019-02-27 15:29:38 +0200

    added a boolean file

---
+++ booleans.java/src/com/company/Main.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(15 == 10); // returns false, because 10 is not equal to 15
+    }
+}
