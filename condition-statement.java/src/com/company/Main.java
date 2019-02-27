704df52274f20e38833f820ba3b0c146e4450906
TCGPC1 <you@example.com>
2019-02-27 15:38:38 +0200

    added a condition statement file

---
+++ condition-statement.java/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+int time = 20;
+if(time <18){
+    System.out.println("good day");
+}else {
+    System.out.print("good evening");
+}
+	// write your code here
+    }
+}
