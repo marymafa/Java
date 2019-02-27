534529ed530ec900a230f91d2c36b1c1c5798179
TCGPC1 <you@example.com>
2019-02-27 11:39:22 +0200

    made changes

---
+++ main.java/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "John ";
+        String lastName = "Doe";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+    }
+}
---
+++ string.java/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int len = txt.length();
+
+        System.out.println("The length of the txt string is: " + len);
+    }
+}
