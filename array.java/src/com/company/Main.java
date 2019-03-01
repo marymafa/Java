83669b6a3d9442e2901804554766ba6edfddd39c
TCGPC1 <you@example.com>
2019-03-01 14:05:08 +0200

    added an array file

--- array.java/src/com/company/Main.java
+++ array.java/src/com/company/Main.java
@@ -4,14 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int count = 0;
 
-        do{
-            System.out.println(count);
+        int [] array= {8,2,3,9,5,5,6,11,7};
 
-            count++;
+        for(int num=0; num<5; num++){
+            System.out.println(array[num]);
         }
-
-        while(count <= 10);
     }
 }
