318378d6aa7a5a07ece5a460f13f971803ba123c
TCGPC1 <you@example.com>
2019-03-01 14:52:29 +0200

    made changes

---
+++ running-multiple-method-in-one-class.java/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        myMethod();
+    }
+    public static void myMethod() {
+        // write your code here
+        System.out.println("this is my first method");
+    }
+}
