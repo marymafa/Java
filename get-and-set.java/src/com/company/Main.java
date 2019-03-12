24f327d3014e196de204642bd032382632abb3fd
TCGPC1 <you@example.com>
2019-03-12 10:42:33 +0200

    added files

---
+++ get-and-set.java/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+Person myobject = new Person();
+myobject.setName("mary");
+        System.out.println(myobject.getName());
+    }
+}
