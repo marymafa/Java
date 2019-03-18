d77f56c0cd02b4757f7839a511dc37958db45d86
TCGPC1 <you@example.com>
2019-03-18 16:08:49 +0200

    added a new code

--- enum.java/src/com/company/Main.java
+++ enum.java/src/com/company/Main.java
@@ -1,20 +1,25 @@
 package com.company;
 
-public class Main {
 
-    enum Level{
-        LOW ,
-        MWDIEM,
+public class Main {
+    enum Level {
+        LOW,
+        MEDIUM,
         HIGH
     }
-
     public static void main(String[] args) {
-	// write your code here
-        Level myVar = Level.LOW;
-        Level myVar1 = Level.HIGH;
-        Level myVar2 = Level.MWDIEM;
-        System.out.println(myVar);
-        System.out.println(myVar1);
-        System.out.println(myVar2);
+        Level myVar = Level.MEDIUM;
+
+        switch(myVar) {
+            case LOW:
+                System.out.println("Low level");
+                break;
+            case MEDIUM:
+                System.out.println("Medium level");
+                break;
+            case HIGH:
+                System.out.println("High level");
+                break;
+        }
     }
 }
