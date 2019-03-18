eebcb04de27c928de842749c2e670f8935eff730
TCGPC1 <you@example.com>
2019-03-18 15:01:03 +0200

    added a new file

---
+++ inteface.java/src/com/company/Animal.java
@@ -0,0 +1,6 @@
+package com.company;
+
+interface Animal {
+    public void animalSound();
+    public void sleep();
+}
---
+++ inteface.java/src/com/company/Pig.java
@@ -0,0 +1,10 @@
+package com.company;
+
+ class Pig implements Animal {
+     public void animalSound(){
+         System.out.println("the pig says: wee wee");
+     };
+     public void sleep(){
+         System.out.println("zzzzz");
+     };
+}
