ec9bb585f8404feaa1410f7f4930d65f75969837
TCGPC1 <you@example.com>
2019-04-10 12:14:03 +0200

    added a new file

---
+++ arrray-lists.java/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("mary");
+        names.add("mercy");
+        System.out.println(names);
+    }
+}
