8127e743db8fca20fe019df0848da0e114cbc729
TCGPC1 <you@example.com>
2019-04-10 12:46:26 +0200

    made changes

---
+++ hashmap.java/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        HashMap<String,String> capitalCities = new HashMap<String, String>();
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+        System.out.println(capitalCities);
+    }
+}
