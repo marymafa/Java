72ada71b7004509374ad0e8b4791dffcacc09b61
TCGPC1 <you@example.com>
2019-04-10 10:34:32 +0200

    added a new file for array

--- binary-chop.java/src/com/company/BinaryChop.java
+++ binary-chop.java/src/com/company/BinaryChop.java
@@ -5,15 +5,18 @@ public class BinaryChop {
     public  static int chop(int search, int[] array) {
 
         int min = 0;
-        int max = array.length - 1;
+        int max = array.length-1;
+//       System.out.println(max);
 
         while( min <= max){
-            int middle =(array.length)/2;
+            int middle = array.length/2;
             if(search < array[middle]){
                 max = middle -1;
+//                System.out.println(middle);
             }
             if(search > array[middle]){
                 min = middle + 1;
+//                System.out.println(min);
             }
             if(search == array[middle]){
                 return middle;
--- binary-chop.java/src/com/company/Main.java
+++ binary-chop.java/src/com/company/Main.java
@@ -3,9 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
         BinaryChop test = new BinaryChop();
-        int []numbers =  {1 ,3,5,7,9 };
-System.out.println(test.chop(5 , numbers));
+
+        int [] numbers =  {1 ,3,5,7,9 };
+
+System.out.println(test.chop(7, numbers));
+
     }
 }
