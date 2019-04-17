9efc66b28e052e41233372b1261e4a7bc5045303
TCGPC1 <you@example.com>
2019-04-17 14:44:32 +0200

    added a methif file

---
+++ method.java/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    static void myMethod( String fname) {
+        System.out.println(fname + " Doe");
+    }
+
+    public static void main(String[] args) {
+        myMethod("John");
+    }
+}
