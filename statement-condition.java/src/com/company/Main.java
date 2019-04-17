aa3187704964cd1d3c05e5ab88a38cbc49b60d2a
TCGPC1 <you@example.com>
2019-04-17 14:52:38 +0200

    added a file

---
+++ statement-condition.java/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    // Create a checkAge() method with an integer variable called age
+    static void  checkAge(int age ) {
+
+        // If age is less than 18, print "Access denied"
+       if (age < 18  ) {
+            System.out.println("Access denied");
+
+            // If age is greater than 18, print "Access granted"
+        }  else{
+            System.out.println("Access granted");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        // Call the checkAge method and pass along an age of 20
+    checkAge(19);
+    }
+
+}
