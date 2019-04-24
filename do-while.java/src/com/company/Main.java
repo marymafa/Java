04a9ce4c152a03ea1fb9bcd9f9c39404d44ea7e0
TCGPC1 <you@example.com>
2019-04-24 10:03:09 +0200

    made changes

---
+++ do-while.java/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+      int count = 0;
+
+      do {
+          System.out.println(count);
+          count++;
+      }
+      while(count < 5);
+    }
+}
---
+++ methods.java/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+	// write your code here
+        int switchValue = 1;
+        switch(switchValue){
+            case 1:
+                System.out.println("value was 1");
+                break;
+            case 2:
+                System.out.println("value was 2 ");
+                break;
+            default:
+                System.out.println("was not 1 or 2");
+                break;
+        };
+
+    }
+}
