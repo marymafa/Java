1e3db21cc8de6235649867d26649d57f4d44813b
TCGPC1 <you@example.com>
2019-05-02 10:22:19 +0200

    made changes

---
+++ example-hash-map.java/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        HashMap<String,String> hash = new HashMap<>();
+        hash.put("England", "London");
+
+        System.out.println(hash);
+
+
+    }
+}
