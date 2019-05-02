7e0e8fe094771b63dd9c33f1dc963b58de1e6bca
TCGPC1 <you@example.com>
2019-05-02 12:53:00 +0200

    added a new file

---
+++ concurency.java/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Integer i = new Integer(10);
+        System.out.println(i);
+        int i1 =i;
+        System.out.println(i1);
+
+        char gfg = 'a';
+        System.out.println(gfg);
+
+char ch = gfg;
+        System.out.println(ch);
+        System.out.println(gfg);
+    }
+}
