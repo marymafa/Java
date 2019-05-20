90a7fd764e0acb31f03aa747e4aac677b3b4a0a0
TCGPC1 <you@example.com>
2019-05-20 13:51:00 +0200

    did some changes in real belling service test

--- Guice/src/test/java/RealBillingServiceTest.java
+++ Guice/src/test/java/RealBillingServiceTest.java
@@ -1,24 +1,17 @@
+import junit.framework.TestCase;
+
 import static org.junit.Assert.*;
 
-public class RealBillingServiceTest {
+public class RealBillingServiceTest extends TestCase {
     private final PizzaOrder order = new PizzaOrder(100);
     private final CreditCard creditCard = new CreditCard("1234", 11, 2010);
 
     private final InMemoryTransactionLog transactionLog = new InMemoryTransactionLog();
     private final FakeCreditCardProcessor processor = new FakeCreditCardProcessor();
 
-    @Override public void setUp() {
-        TransactionLogFactory.setInstance(transactionLog);
-        CreditCardProcessorFactory.setInstance(processor);
-    }
-
-    @Override public void tearDown() {
-        TransactionLogFactory.setInstance(null);
-        CreditCardProcessorFactory.setInstance(null);
-    }
-
     public void testSuccessfulCharge() {
-        RealBillingService billingService = new RealBillingService();
+        RealBillingService billingService
+                = new RealBillingService(processor, transactionLog);
         Receipt receipt = billingService.chargeOrder(order, creditCard);
 
         assertTrue(receipt.hasSuccessfulCharge());
@@ -27,5 +20,4 @@ public class RealBillingServiceTest {
         assertEquals(100, processor.getAmountOfOnlyCharge());
         assertTrue(transactionLog.wasSuccessLogged());
     }
-
 }
