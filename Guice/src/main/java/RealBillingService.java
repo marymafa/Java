17f09ed90ac4a91cf947cb91a70d8f11bc5fe545
TCGPC1 <you@example.com>
2019-05-20 09:31:17 +0200

    implemented billing service in real billing service

--- Guice/src/main/java/RealBillingService.java
+++ Guice/src/main/java/RealBillingService.java
@@ -1,2 +1,18 @@
-public class RealBillingService {
+public class RealBillingService implements BillingService {
+    public Receipt chargeOrder(PizzaOrder order, CreditCard creditCard) {
+        CreditCardProcessor processor = new PaypalCreditCardProcessor();
+        TransactionLog transactionLog = new DatabaseTransactionLog();
+
+        try {
+            ChargeResult result = processor.charge(creditCard, order.getAmount());
+            transactionLog.logChargeResult(result);
+
+            return result.wasSuccessful()
+                    ? Receipt.forSuccessfulCharge(order.getAmount())
+                    : Receipt.forDeclinedCharge(result.getDeclineMessage());
+        } catch (UnreachableException e) {
+            transactionLog.logConnectException(e);
+            return Receipt.forSystemFailure(e.getMessage());
+        }
+    }
 }
