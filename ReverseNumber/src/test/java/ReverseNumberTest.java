3eb8da12906885e8910a04e77f3350251f9c9fb8
TCGPC1 <you@example.com>
2019-05-30 10:33:36 +0200

    added a unit test

--- ReverseNumber/src/test/java/ReverseNumberTest.java
+++ ReverseNumber/src/test/java/ReverseNumberTest.java
@@ -1,15 +1,17 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ReverseNumberTest {
 
-
+    @Test
     public void reverse() throws Exception {
-        assertEquals( 321, ReverseNumber.reverse( String.valueOf( 123 ) ) );
-        assertEquals( -321, ReverseNumber.reverse( String.valueOf( -123 ) ) );
-        assertEquals( 1, ReverseNumber.reverse( String.valueOf( 1000 ) ) );
-        assertEquals( 4321234, ReverseNumber.reverse( String.valueOf( 4321234 ) ) );
-        assertEquals( 5, ReverseNumber.reverse( String.valueOf( 5 ) ) );
-        assertEquals( 89898989, ReverseNumber.reverse( String.valueOf( 98989898 ) ) );
+        assertEquals( 321, ReverseNumber.reverse( 123 ) );
+        assertEquals( -321, ReverseNumber.reverse( -123 ) );
+        assertEquals( 1, ReverseNumber.reverse( 1000 ) );
+        assertEquals( 4321234, ReverseNumber.reverse( 4321234 ) );
+        assertEquals( 5, ReverseNumber.reverse( 5 ) );
+        assertEquals( 89898989, ReverseNumber.reverse( 98989898 ) );
     }
 
 
